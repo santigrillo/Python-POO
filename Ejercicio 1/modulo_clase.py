98f940f99b90c990150d6243c4877a6c6d294cec
grillosanti <admsanti90@gmail.com>
2023-04-30 22:31:50 -0300

    Ejercicio 1 Final

--- Ejercicio 1/modulo_clase.py
+++ Ejercicio 1/modulo_clase.py
@@ -4,17 +4,31 @@ class email:
     __tipoDominio = ''
     __password = ''
     
-    def __init__(self, xid, xdominio, xtipo, xpassword):
+    def __init__(self, xid=None, xdominio=None, xtipo=None, xpassword=None):
         self.__idcuenta = xid
         self.__dominio = xdominio
         self.__tipoDominio = xtipo
-        self.__password = xpassword 
+        self.__password = xpassword
         
-    def returncorreo(self):
-       print(self.__idcuenta+self.__dominio+self.__tipoDominio) 
-   
+    def __str__ (self):
+        return(self.__idcuenta + "@" + self.__dominio + "." + self.__tipoDominio)
+        
+    
+    def createAccount(self,correo,cont):
+        if (correo.find('@')!= -1):
+            auxCorreo= correo.split('@')
+            user = auxCorreo[0]
+            if (auxCorreo[1].find('.')!= -1):
+                domCompleto= auxCorreo[1].split('.')
+                auxDominio= domCompleto[0]
+                auxTipoDominio= domCompleto[1]
+                self.__init__(user,auxDominio,auxTipoDominio,cont)
+                print('Correo creado con exito')
+            else: print('Error: El dominio no contiene punto')
+        else: print ('Error: El correo ingresado no contiene @') 
+    
     def getDominio(self):
-        return("El dominio es "+self.__dominio)      
+        return("El dominio es "+self.__dominio)     
         
     def changePassword(self):
         print ("Modulo cambio contraseña")
--- Ejercicio 1/modulo_main.py
+++ Ejercicio 1/modulo_main.py
@@ -1,48 +1,25 @@
 from modulo_clase import email
 import csv
 
-def crearcorreo (correo):
-     cadena = correo.split("@")
-     nuevoid = cadena[0]
-     cadena2 = cadena[1].split(".")
-     nuevodominio = "@" + cadena2[0]
-     tipodominio = "." + cadena2[1]
-     passwordnueva = input("Ingrese clave: ")
-     correonuevo = email(nuevoid, nuevodominio, tipodominio, passwordnueva)
-     return correonuevo
-
-def cargacorreo(listacorreos, long):
-    for i in range(long):
-     correo = input("Ingrese su correo electronico -> ")
-     c = crearcorreo(correo)
-     listacorreos.append(c)
-     
-def contdominio():
-     cont = 0
-     correos = open('correos.csv', 'r')
-     dominioSearch = input("Ingrese dominio a buscar: ")
-     for fila in correos:
-          if(dominioSearch in fila):
-               cont += 1
-     print("Hay ", cont, " correos con ese dominio")
-
 if __name__ == '__main__':
-    
+     listaCorreos = []
+     mail=email()
      nombre = input("Bienvenido, ingrese su nombre: ")
-     xid = input("Ingrese ID: ")
-     xdominio = input("Ingrese Dominio (gmail, yahoo, etc.): ")
-     xtipo = input("Ingrese tipo (.com, .uk, .es): ")
+     xcorreo = input("Ingrese su correo: ")
      xpassword = input("Ingrese su contraseña: ")
-    
-     santiago=email(xid, xdominio, xtipo, xpassword)
-     print ("El correo se ha creado con exito")    
-    
-     print ("Bienvenido ", nombre, " te enviaremos tus mensajes a la dirección ", santiago.returncorreo())
+     mail.createAccount(xcorreo, xpassword)
      
-     santiago.changePassword()
+     print ("El correo se ha creado con exito")
+         
+     print ("Bienvenido ", nombre, " te enviaremos tus mensajes a la dirección ", mail)
      
-     contdominio()
+     mail.changePassword()    
           
-
-     
-     
+     with open('correos.csv', 'r') as file:
+          file_reader = csv.reader(file, delimiter=';')
+          cont = 0
+          dominiox = input("Dominio a buscar > ")
+          for i in file_reader:
+               if (dominiox in i[0]):
+                    cont += 1
+          print("Hay ", cont, " correos con ese dominio")
